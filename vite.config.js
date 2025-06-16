f445be63ad2b18242d9a83691f774658973a7f95
ClarkWilliamsIE <clark@inertiaed.org>
2025-06-16 16:28:38 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Library-Calander/',
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/library-schedule-display/' : '/'
 })
